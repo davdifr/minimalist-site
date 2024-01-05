7b93ea6129246891bf2787c6e797ff395ec442a7
Davide Di Francesco <davdifr@gmail.com>
2024-01-05 18:11:50 +0100

    not-found component theme injection and RouterModule

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,14 +1,20 @@
 import { Component, inject } from '@angular/core';
 import { LoggerService } from '../../shared/services/logger.service';
+import { RouterModule } from '@angular/router';
+import { ThemeService } from '../../shared/services/theme.service';
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   styleUrl: './not-found.component.css',
-  template: `<h1>404 - Not Found</h1>`,
+  template: `<h1>404 - Not Found</h1>
+    <p>
+      <a routerLink="/">Go Home</a>
+    </p>`,
 })
 export class NotFoundComponent {
+  private theme = inject(ThemeService);
   private logger = inject(LoggerService);
 
   ngOnInit(): void {
